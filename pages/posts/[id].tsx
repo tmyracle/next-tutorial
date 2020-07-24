8c8dd151baa1255f28aa86292450bf2a039b8b13
Tyler Myracle <tyler.myracle@gmail.com>
2020-07-24 16:18:07 -0700

    add post bg

--- pages/posts/[id].tsx
+++ pages/posts/[id].tsx
@@ -19,7 +19,7 @@ export default function Post({
       <Head>
         <title>{postData.title}</title>
       </Head>
-      <article>
+      <article className="bg-white rounded-lg shadow-md p-6">
         <h1 className={utilStyles.headingXl}>{postData.title}</h1>
         <div className={utilStyles.lightText}>
           <Date dateString={postData.date} />
